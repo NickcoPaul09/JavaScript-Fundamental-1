26db269cedc824489adf2cbcab6dcca551baed3a
Nickco Paul <nickcopaul@gmail.com>
2023-06-10 00:18:01 +0100

    JS Fundamental Part 1 JavaScript Values & Variables

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 let js = "amazing";
-
-if (js === "amazing") alert("JavaScript is Fun!");
-
 12 + 10 + 44 - 10;
 console.log(12 + 10 + 44 - 10);
 
 //This is JavaScript journey begin
+
+//JavaScript's Values & Variables
+console.log("Nickco");
+console.log(22);
+
+let firstName = "Nickco";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//Variables name cannot start the number
+// let 3years = 3;  **we can use number infront of variable name**
+
+// We can give variable name like this
+
+let $function = 23;
+let first_name = "Paul";
+let person = "Nickco";
+let PI = 1.1415;
+
+// Do & Don't when you give variable names
+
+// Do
+let firstJob = "Programmer";
+let lastJob = "Teacher";
+
+// Don't
+let job1 = "Programmer";
+let job2 = "Teacher";
